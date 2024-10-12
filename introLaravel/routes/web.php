6738f1ec224bb85f4f7d9c379ab7d780bbf9e848
Roberto Resendiz <robertoresendiz504@gmail.com>
2024-10-11 18:26:20 -0600

    Practica 4 Finalizada

--- introLaravel/routes/web.php
+++ introLaravel/routes/web.php
@@ -1,15 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
 
 /* Route::get('/', function () {
     return view('welcome');
 }); */
 
-Route::view('/', 'inicio')->name('rutainicio');
+// Route::view('/', 'inicio')->name('rutainicio');
 
-Route::view('/formulario', 'formulario')->name('rutaformulario');
+// Route::view('/formulario', 'formulario')->name('rutaformulario');
 
-Route::view('/cliente', 'cliente')->name('rutaclientes');
+// Route::view('/cliente', 'cliente')->name('rutaclientes');
 
 Route::view('/componentes', 'componentes')->name('rutacomponentes');
+
+
+
+
+
+
+// Rutas para trabajar con controladores
+Route::get('/', [controladorVistas::class,'home'])->name('rutainicio');
+
+Route::get('/formulario', [controladorVistas::class,'formulario'])->name('rutaformulario');
+
+Route::get('/cliente', [controladorVistas::class,'consulta'])->name('rutaclientes');
