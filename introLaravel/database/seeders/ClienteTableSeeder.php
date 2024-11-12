e48adab74148ffdd478c695ce4e7b4ac9014a5a7
Roberto Resendiz <robertoresendiz504@gmail.com>
2024-11-11 19:01:57 -0600

    Avance practica 7

---
+++ introLaravel/database/seeders/ClienteTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\BD;
+
+class ClienteTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('clientes')->insert([
+            [
+                'nombre'=>'Ivan',
+                'apellido'=>'Gerra',
+                'Correo'=>'iiguerra@gmail.com',
+                'telefono'=>'23456783373'
+            ],
+            [
+                'nombre'=>'Robert',
+                'apellido'=>'Resendiz',
+                'Correo'=>'roberto@gmail.com',
+                'telefono'=>'25656783373'
+            ],
+            [
+                'nombre'=>'Jera',
+                'apellido'=>'Perez',
+                'Correo'=>'perezj@gmail.com',
+                'telefono'=>'23456784373'
+            ]]);
+    }
+}
